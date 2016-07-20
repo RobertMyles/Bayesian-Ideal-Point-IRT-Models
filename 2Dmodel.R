5cf863a638cad97b3c9aec88e09d760399a76b40
Robert Myles McDonnell <mcdonnell.robert5@gmail.com>
2016-07-20 19:33:41 -0300

    Update 2d Model

--- 2Dmodel.R
+++ 2Dmodel.R
@@ -5,29 +5,39 @@ library(rstan)
 library(coda)
 library(plyr)
 library(dplyr)
+library(readr)
+
 
 # The following assumes that you have a dataset called "data", of votes and legislator details
 # 'FullID' refers to the legislator name and party; 'VoteNumber' to the number associated with the
 # vote. 'Vote' is the actual vote of each legislator. It should be numeric and in the form 'Yes'=1;
 # 'No' = 0; absent, NA = NA.
 
+# If you don't have this type of data already, we can use this example:
+
+VoteData  <-  read_csv("https://github.com/RobertMyles/R-Scripts-Bayesian-Ideal-Point-IRT/blob/master/Senate_Example.csv?raw=true")
 
 # build the vote matrix
-NameID <- unique(data$FullID)
+VoteData$FullID <- paste(VoteData$SenatorUpper, VoteData$Party, sep=":")
+NameID <- unique(VoteData$FullID)
 J <- length(unique(NameID))
-K <- length(unique(data$VoteNumber))
+K <- length(unique(VoteData$VoteNumber))
+
+VoteData$Vote[VoteData$Vote=="S"] <- 1
+VoteData$Vote[VoteData$Vote=="N"] <- 0
+VoteData$Vote[VoteData$Vote %in% c("O", "A", NA)] <- NA
 
 y <- matrix(NA, J, K)
-Rows <- match(data$FullID, NameID)
-Cols <- unique(data$VoteNumber)
-Columns <- match(data$VoteNumber, Cols)
+Rows <- match(VoteData$FullID, NameID)
+Cols <- unique(VoteData$VoteNumber)
+Columns <- match(VoteData$VoteNumber, Cols)
 
-for(i in 1:dim(data)[1]){
-  y[Rows[i],Columns[i]] <- data$Vote[i]
+for(i in 1:dim(VoteData)[1]){
+  y[Rows[i],Columns[i]] <- VoteData$Vote[i]
 }
 
 y <- matrix(as.numeric(unlist(y)),nrow=nrow(y))
-dimnames(y) <- list(unique(NameID), unique(data$VoteNumber))
+dimnames(y) <- list(unique(NameID), unique(VoteData$VoteNumber))
 
 # The vote matrix is organised so that the constraints appear in the first two rows and 
 # columns (just for convenience).
@@ -39,40 +49,45 @@ dimnames(y) <- list(unique(NameID), unique(data$VoteNumber))
 # because the use of truncations (such as: theta[1,1] ~  normal(1, 1) t[0,]; ) can lead to 
 # lots of divergent transitions in Stan if you don't have informative data. But you can just switch the
 # constraints to the line above if you prefer softer constraints.
+# 
+# You also don't have to fix the vote parameters like I do here, you can fix (or constrain) the position 
+# of three legislators in the 2D space if you have good theoretical knowledge as to where they 
+# should be located. In one dimension, this is usually straightforward, it becomes more difficult
+# in more dimensions.
 
-# Legislator data (for graphing, assumes you have all this info!):
+# Legislator data (for graphing and further analysis):
 
-ldata <- data.frame(FullID=unique(NameID), Party=data$Party[match(unique(NameID), data$FullID)], 
-          GovCoalition=data$GovCoalition[match(unique(NameID), data$FullID)],
-          Name=data$Name[match(unique(NameID), data$FullID)], 
-          State=data$State[match(unique(NameID), data$FullID)], 
-          row.names=NULL, stringsAsFactors=FALSE)
+ldata <- data_frame(FullID = unique(NameID), 
+          Party = VoteData$Party[match(unique(NameID), VoteData$FullID)], 
+          GovCoalition=VoteData$GovCoalition[match(unique(NameID), VoteData$FullID)],
+          Name=VoteData$SenatorUpper[match(unique(NameID), VoteData$FullID)], 
+          State=VoteData$State[match(unique(NameID), VoteData$FullID)])
 
 ########################################################################################################
 #  Model in Stan:
 
 stan.code <- "
 data {
-  int<lower=1> J; //Legislators
-  int<lower=1> K; //Proposals
-  int<lower=1> N; //no. of observations
-  int<lower=1, upper=J> j[N]; //Legislator for observation n
-  int<lower=1, upper=K> k[N]; //Proposal for observation n
-  int<lower=0, upper=1> y[N]; //Vote of observation n
-  int<lower=1> D; //No. of dimensions
+  int<lower=1> J;                 //Legislators
+  int<lower=1> K;                 //Proposals
+  int<lower=1> N;                 //no. of observations
+  int<lower=1, upper=J> j[N];     //Legislator for observation n
+  int<lower=1, upper=K> k[N];     //Proposal for observation n
+  int<lower=0, upper=1> y[N];     //Vote of observation n
+  int<lower=1> D;                 //No. of dimensions
 }
 parameters {
-  real alpha[K]; //difficulty
-  matrix[K,D] beta; //discrimination
-  matrix[J,D] theta; //latent trait (ideal points)
+  real alpha[K];                    //difficulty (intercept)
+  matrix[K,D] beta;                 //discrimination (slope)
+  matrix[J,D] theta;                //latent trait (ideal points)
 }
 model {
   alpha ~ normal(0,10); 
   to_vector(beta) ~ normal(0,10); 
   to_vector(theta) ~ normal(0,1); 
-  theta[1,1] ~  normal(1, .01); //constraints, ideal points, dimension 1
+  theta[1,1] ~  normal(1, .01);     //constraints, ideal points, dimension 1
   theta[2,1] ~ normal(-1, .01);  
-  beta[1,2] ~ normal(-4, 2); // beta constraints
+  beta[1,2] ~ normal(-4, 2);        // beta constraints
   beta[2,2] ~ normal(4, 2); 
   beta[1,1] ~ normal(0, .1); 
   beta[2,1] ~ normal(0, .1); 
@@ -99,7 +114,7 @@ stan.data <- list(J=J, K=K, N=N, j=j, k=k, y=y, D=D)
 # Stan run:
 # No inits here, but straightforward to include
 
-stan.fit <- stan(model_code=stan.code, data=stan.data, iter=5000, warmup=2500, chains=4, thin=20, verbose=TRUE, cores=4, seed=1234)
+stan.fit <- stan(model_code=stan.code, data=stan.data, iter=100, warmup=25, chains=4, verbose=TRUE, cores=4, seed=1234)
 
 # Some diagnostics
 stan_rhat(stan.fit, bins=60)
@@ -129,10 +144,5 @@ Theta <- merge(Theta, ldata, by="FullID")
 find_hull <- function(Theta) Theta[chull(Theta$MeanD1, Theta$MeanD2), ]
 hulls <- ddply(Theta, "GovCoalition", find_hull)
 
-ggplot(Theta, aes(x=MeanD1, y=MeanD2, colour=GovCoalition, fill=GovCoalition)) 
-        + geom_point(shape=19, size=3) + geom_polygon(data=hulls, alpha=.18) 
-        + geom_text(aes(y=MeanD2, label=FullID, colour=GovCoalition), size=2.5, vjust=-0.75) 
-        + scale_colour_manual(values=c("red", "blue")) + scale_fill_manual(values=c("red", "blue")) 
-        + theme_bw() 
-        + theme(axis.text.y=element_blank(), axis.ticks.y=element_blank(), axis.title=element_blank(), legend.position="none") 
-        + scale_x_continuous(limits=c(-1.9, 2.25)) #if necessary to adjust scale
+ggplot(Theta, aes(x=MeanD1, y=MeanD2, colour=GovCoalition, fill=GovCoalition)) + geom_point(shape=19, size=3) + geom_polygon(data=hulls, alpha=.18) + geom_text(aes(y=MeanD2, label=FullID, colour=GovCoalition), size=2.5, vjust=-0.75) + scale_colour_manual(values=c("red", "blue")) + scale_fill_manual(values=c("red", "blue")) + theme_bw() + theme(axis.text.y=element_blank(), axis.ticks.y=element_blank(), axis.title=element_blank(), legend.position="none") 
+        #+ scale_x_continuous(limits=c(-1.9, 2.25)) #if necessary to adjust scale
